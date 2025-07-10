60b070182c9308a62c0a4e9159f0ebb77d8e6d09
alanledur1 <ledur77conta2@gmail.com>
2025-07-10 18:07:12 -0300

    atualization URL API city #2

--- src/services/apiCity.jsx
+++ src/services/apiCity.jsx
@@ -64,7 +64,7 @@ function ApiResultClima({ onDataFetch}) {
         setError(null);
 
         try {
-            const response = await axios.get(`https://climaanalizerserver.rj.r.appspot.com/api/clima/clima-cidade`, {
+            const response = await axios.get(`https://climaanalizerserver.rj.r.appspot.com/api/clima-cidade`, {
                 params: { cidade: city }
             });
             setData(response.data);
