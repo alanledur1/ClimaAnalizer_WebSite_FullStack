0c26d587665b72e32f67b2cfed721d9082d5cc82
alanledur1 <ledur77conta2@gmail.com>
2025-07-16 12:10:13 -0300

    alteracao rota #2

--- src/services/apiCity.jsx
+++ src/services/apiCity.jsx
@@ -64,7 +64,7 @@ function ApiResultClima({ onDataFetch}) {
         setError(null);
 
         try {
-            const response = await axios.get(`https://api-climaanalizer.onrender.com/clima-cidade`, {
+            const response = await axios.get(`https://api-climaanalizer.onrender.com/api/clima-cidade`, {
                 params: { cidade: city }
             });
             setData(response.data);
